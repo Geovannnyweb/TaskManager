a695006a4704ec2ccfaf236cbb65e36f1b3435b8
Geovanny Camargo <giovani.camargor@opitech.com.co>
2024-02-26 16:40:16 -0500

    feat(task): Models and Enums Added

---
+++ TaskManager.Task.Domain/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using UserEntity =  TaskManager.User.Domain.Models.User;
+
+namespace TaskManager.Task.Domain.Models
+{
+    public class Task
+    {
+        public Guid TaskId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public TaskStatus Status { get; set; }
+        public Guid UserId { get; set; }
+        public UserEntity? Users { get; set; }
+    }
+}
---
+++ TaskManager.User.Domain/Enums/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.User.Domain.Enums
+{
+    public enum Role
+    {
+        Admin,
+        TaskManager
+    }
+}
