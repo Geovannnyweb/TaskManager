6b75839d8631c00469d45360eff30c3fa27a6d5b
Geovanny Camargo <giovani.camargor@opitech.com.co>
2024-02-26 17:24:52 -0500

    feat(task): DbContext Added

---
+++ TaskManager.Task.Infrastructure/Services/TaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskEntity = TaskManager.Task.Domain.Models.Task;
+
+namespace TaskManager.Task.Infrastructure.Services
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<TaskEntity> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          
+        }
+    }
+
+
+}
